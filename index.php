fb8ac4ed357abe41ef3b216dfba5448dccdb271e
Indro <indro.malleus@khk.ee>
2015-10-04 14:40:20 +0300

    updated

---
+++ index.php
@@ -0,0 +1,240 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>All tag project</title>
+
+    <meta charset="UTF-8">
+    <meta name="description" content="Indro">
+    <meta name="keywords" content="HTML,CSS">
+    <meta name="author" content="website">
+
+
+</head>
+<body>
+
+<link rel="stylesheet" href="stylesheet.css">
+
+<a href="index.php">Link </a>
+
+back to this <abbr title="All Tag Project"><em>Page</em></abbr>
+
+
+<header>
+<h1>header 1 - ALL TAG PROJECT - header 1</h1>
+</header>
+
+<h2>header 2</h2>
+
+<h3>header 3</h3>
+
+<h4>header 4</h4>
+
+<h5>header 5</h5>
+
+<h6>header 6</h6>
+
+<main>
+<article>
+    <h1 style="font-size: 20px;font-family: monospace">Tags Project</h1>
+    <p style="font-family: monospace;">Made by me</p>
+</article>
+</main>
+
+
+<address style="float: right">
+    Contact <a href="mailto:indro.malleus@khk.ee">Indro</a>.<br>
+</address>
+
+<img src="Troll-face.jpg" width="200" height="200" alt="Planets"
+     usemap="#planetmap">
+
+<map name="planetmap">
+    <area shape="rect" coords="0,0,82,126" href="index.php" alt="Sun">
+    <area shape="circle" coords="90,58,3" href="index.php" alt="Mercury">
+    <area shape="circle" coords="124,58,8" href="index.php" alt="Venus">
+</map>
+
+<p>Lorem ipsum dolor <b> sit amet,</b>, consectetur adipisicing elit. Aut dolore eius ex fuga fugiat, id illo impedit in officia officiis optio quasi quia repellendus reprehenderit rerum tempore ullam, vero voluptas!</b></p>
+
+<aside>
+    <h4>using aside here</h4>
+    <p>What does aside do?</p>
+</aside>
+
+<audio controls>
+    <source src="horse.ogg" type="audio/ogg">
+    <source src="horse.mp3" type="audio/mpeg">
+    Your browser does not support the audio element.
+</audio>
+
+    <base hef="http://www.google.com/" target="_blank">
+    <bdi>This is my audio</bdi>
+    <bdo>ah</bdo>
+
+    <blockquote>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto at autem cumque cupiditate debitis dolores dolorum, enim esse exercitationem ipsa magni minima, molestias nisi numquam officia quibusdam quidem quisquam recusandae?</blockquote>
+
+    <canvas id="myCanvas" width="200" height="100" style="border:1px solid #aedaff;">BOX</canvas>
+
+<table>
+    <caption>table</caption>
+
+    <colgroup>
+        <col span="2" style="background-color:#f4f0ff">
+        <col style="background-color:#abd3ff">
+    </colgroup>
+    <tr>
+        <th>hello  <dialog open>Avatud dialoogi tag.</dialog> </th>
+        <th>this</th>
+    </tr>
+    <tr>
+        <td>is</td>
+        <td>table</td>
+    </tr>
+</table>
+<br>
+<cite>Next topic</cite><br>
+
+<code>A piece of computer code</code>
+<br>
+<input list="browsers">     <button>search</button><br>
+
+<datalist id="browsers">
+    <option value="Internet Explorer">
+    <option value="Firefox">
+    <option value="Chrome">
+    <option value="Opera">
+    <option value="Safari">
+</datalist>
+
+<br>
+<div>
+<dl style="float:left">
+    <dt>Source</dt>
+    <dd>Yes</dd>
+    <dt>Code</dt>
+    <dd>No</dd>
+</dl>
+</div>
+<p>My favorite <i>color</i> is <del><font size="3" color="red">RED</font></del> <ins><font size="3" color="BLUE">BLUE</font></ins>!</p>
+
+
+<p><dfn>HTML</dfn> ja CSS</p>
+
+<form>
+    <fieldset>
+        <legend>Sisesta oma andmed:</legend>
+        Name: <input type="text"><br>
+        Email: <input type="text"><br>
+        Date of birth: <input type="text">
+    </fieldset>
+</form>
+
+<figure>
+    <img src="Troll-face.jpg" alt="Trollface" width="228" height="228">
+    <figcaption>Trollface - meme</figcaption>
+</figure>
+
+<footer>
+    <p>Kasutan Footerit</p>
+    <p>Contact information: <a href="mailto:indro.malleus@khk.ee">
+            indro</a>.</p>
+</footer>
+
+
+<iframe src="http://www.w3schools.com"></iframe>
+
+<form action="demo_form.asp">
+    Eesnimi: <input type="text" name="fname"><br>
+    Perekonnanimi: <input type="text" name="lname"><br>
+    <input type="submit" value="Submit">
+</form>
+<br>
+<br>
+<ins>ins tag here</ins>
+<br>
+<kbd>Keyboard input TAG</kbd>
+<br>
+
+<form action="demo_keygen.asp" method="get" style="float:left">
+    Username: <input type="text" name="usr_name">
+    Encryption: <keygen name="security">
+        <input type="submit">
+</form>
+
+<form action="demo_form.asp" style="float:right">
+    <label for="male">Male</label>
+    <input type="radio" name="sex" id="male" value="male"><br>
+    <label for="female">Female</label>
+    <input type="radio" name="sex" id="female" value="female"><br>
+    <input type="submit" value="Submit">
+</form>
+<br>
+<br>
+<br>
+<br>
+
+<p>using <mark>mark</mark> tag.</p>
+<br>
+<br>
+
+
+<p>using meter tag:</p>
+<meter value="0.6">60%</meter>
+
+<br>
+<br>
+<br>
+
+
+<nav>
+    <a href="/test/">using</a> |
+    <a href="/test/">nav</a> |
+    <a href="/test/">tag</a> |
+    <a href="/test/">here</a>
+</nav>
+<br>
+
+<noscript>Your browser does not support JavaScript!</noscript>
+<br>
+
+<ol>ordered list</ol>
+
+<br>
+
+<object width="100" height="100">this is object</object>
+<br>
+<br>
+<optgroup label="optgroup" style="float:right">
+    <option value="volvo">tag</option>
+    <option value="saab">tag2</option>
+</optgroup>
+<br>
+<br>
+
+<form oninput="x.value=parseInt(a.value)+parseInt(b.value)">0
+    <input type="range" id="a" value="50">100
+    +<input type="number" id="b" value="50">
+    =<output name="x" for="a b"></output>
+</form>
+
+<br>
+
+<pre>this is pre text <tag></tag></pre>
+
+<br>
+
+<progress value="52" max="100"></progress>
+
+<br>
+
+
+<hr>
+
+<details>
+    <summary>Copyright 2015.</summary>
+    <p> - by indro. All Rights Reserved.</p>
+    <p>All content and graphics created by me!</p>
+</details>
+
+</body>
+</html>
